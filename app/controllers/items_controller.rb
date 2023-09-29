6ca94c831786d11c71f77575340670ca12747119
07ty <rstoyo.yuri.tsushima@gmail.com>
2023-09-29 16:20:55 +0900

    summary

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
     
     def index
       @items = Item.order("created_at DESC") 
@@ -22,6 +22,24 @@ class ItemsController < ApplicationController
     private
 
     def item_params
-      params.require(:item).permit(:item, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
+      params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
+    end
+      
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def edit
+      unless user_signed_in? && current_user == @item.user
+        redirect_to action: :index        
+      end
+    end
+
+    def update
+      if @item.update(item_params)
+        redirect_to item_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
   end
